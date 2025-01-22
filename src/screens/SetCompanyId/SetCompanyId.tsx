099bb6c71f15910b704f480b85bfa66900298f12
MarioGoDevLike <marionassar57@gmail.com>
2025-01-23 00:31:48 +0200

    added set company screen

--- src/screens/SetCompanyId/SetCompanyId.tsx
+++ src/screens/SetCompanyId/SetCompanyId.tsx
@@ -1,10 +1,19 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
+import Button from '../../Components/Button/Button';
+import {useNavigation} from '@react-navigation/native';
+import {Routes} from '../../types/type';
+import {styles} from './styles';
 
 const SetCompanyId = () => {
+  const navigation = useNavigation();
+  const onEnterCompanyIdPress = useCallback(() => {
+    navigation.navigate(Routes.enterCompanyIdScreen as never);
+  }, []);
+
   return (
-    <View>
-      <Text>SetCompanyId</Text>
+    <View style={styles.setCompanyIdContainer}>
+      <Button text="Enter Company ID" onPress={onEnterCompanyIdPress} />
     </View>
   );
 };
