53064a5592b86a484375bc7307688aaf3a004964
MarioGoDevLike <marionassar57@gmail.com>
2025-01-22 21:51:13 +0200

    added main screen

--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,10 +1,34 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import React, {useCallback} from 'react';
+import {styles} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {Routes} from '../../types/type';
+import Button from '../../Components/Button/Button';
 
 const MainScreen = () => {
+  const navigation = useNavigation();
+
+  const onSettingsPress = useCallback(() => {
+    navigation.navigate(Routes.settingsStack as never);
+  }, []);
+
+  const onLaunchBotPress = useCallback(() => {
+    navigation.navigate(Routes.voiceBotScreen as never);
+  }, []);
+
   return (
-    <View>
-      <Text>MainScreen</Text>
+    <View style={styles.mainScreenContainer}>
+      <View style={styles.settingsIconContainer}>
+        <Pressable onPress={onSettingsPress}>
+          <Image
+            source={require('../../assets/pngs/assets/images/settings_icon.png')}
+            style={styles.settingsIcon}
+          />
+        </Pressable>
+      </View>
+      <View style={styles.launchBotContainer}>
+        <Button text="Launch Voicebot Screen" onPress={onLaunchBotPress} />
+      </View>
     </View>
   );
 };
---
+++ src/screens/MainScreen/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  settingsIconContainer: {},
+  settingsIcon: {
+    width: 50,
+    height: 50,
+  },
+  mainScreenContainer: {
+    flex: 1,
+  },
+  launchBotContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
