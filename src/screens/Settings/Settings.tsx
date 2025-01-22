4fe0aecb130dee2956fb1ecb87492c3d60c2296f
MarioGoDevLike <marionassar57@gmail.com>
2025-01-23 00:32:51 +0200

    added settings screen

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,10 +1,24 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
+import {styles} from './styles';
+import Button from '../../Components/Button/Button';
+import {useNavigation} from '@react-navigation/native';
+import {Routes} from '../../types/type';
 
 const Settings = () => {
+  const navigation = useNavigation();
+  const onPickVoiceScreenPress = useCallback(() => {
+    navigation.navigate(Routes.pickVoiceScreen as never);
+  }, []);
+
+  const onCompanyIdPress = useCallback(() => {
+    navigation.navigate(Routes.setCompanyIdScreen as never);
+  }, []);
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={styles.settingsContainer}>
+      <Button text="Pick Voice Screen" onPress={onPickVoiceScreenPress} />
+      <Button text="Set Company ID" onPress={onCompanyIdPress} />
     </View>
   );
 };
---
+++ src/screens/Settings/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  settingsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
