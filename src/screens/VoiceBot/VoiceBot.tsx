034540316028bf57396862cfe75140b0751ed843
MarioGoDevLike <marionassar57@gmail.com>
2025-01-22 21:50:37 +0200

    added voice bot screen

--- src/screens/VoiceBot/VoiceBot.tsx
+++ src/screens/VoiceBot/VoiceBot.tsx
@@ -1,10 +1,18 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {styles} from './styles';
+import Button from '../../Components/Button/Button';
 
 const VoiceBot = () => {
+  const navigation = useNavigation();
+
+  const dismissVoiceBot = () => {
+    navigation.goBack();
+  };
   return (
-    <View>
-      <Text>VoiceBot</Text>
+    <View style={styles.voiceBotContainer}>
+      <Button text="DISMISS" onPress={dismissVoiceBot} />
     </View>
   );
 };
