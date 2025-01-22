d15dcf28d8e316bc1f0a81455f9481040e1aaa36
MarioGoDevLike <marionassar57@gmail.com>
2025-01-22 21:50:46 +0200

    added pick voice screen

--- src/screens/PickVoice/PickVoice.tsx
+++ src/screens/PickVoice/PickVoice.tsx
@@ -1,10 +1,23 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {styles} from './styles';
+import {useAtom} from 'jotai';
+import {companyIdAtom} from '../../atoms';
+import Button from '../../Components/Button/Button';
+import {StackActions, useNavigation} from '@react-navigation/native';
+import {Routes} from '../../types/type';
 
 const PickVoice = () => {
+  const navigation = useNavigation();
+  const [company] = useAtom(companyIdAtom);
+  const onConfirmNavigate = () => {
+    navigation.dispatch(StackActions.replace(Routes.mainScreenStack));
+  };
+
   return (
-    <View>
-      <Text>PickVoice</Text>
+    <View style={styles.pickVoiceContainer}>
+      <Text style={styles.companyIdText}>Company ID Provided: {company}</Text>
+      <Button text="Dismiss" onPress={onConfirmNavigate} />
     </View>
   );
 };
---
+++ src/screens/PickVoice/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  pickVoiceContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  companyIdText: {
+    fontSize: 15,
+    fontWeight: '500',
+  },
+});
